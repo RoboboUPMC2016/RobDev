efa3da9171c48af5f06ea23870d34e02629ffff1
clementgeorge <clementyj.george@gmail.com>
2016-11-25 18:48:44 +0100

    corrects a little mistake

--- src/robdev/Actions.java
+++ src/robdev/Actions.java
@@ -5,56 +5,56 @@ package robdev;
  * create amazing robot behaviors.
  */
 public interface Actions {
-    
+
 	/**
 	 * Keeps the robot waiting for a while
 	 * @param seconds : the number of seconds to wait
 	 */
-    public void wait(int seconds);
-    
-    /**
-     * Keeps the robot waiting for a while
-     * @param event : an event to wait to resume the behavior
-     */
-    public void wait(Events event);
+	public void wait(int seconds);
 
-    /**
-     * Moves the robot forward
-     * @param seconds : the number of seconds to move
-     */
-    public void moveForward(int seconds);
-    
-    /**
-     * Moves the robot forward
-     * @param event : an event to wait to go on the next step
-     */
-    public void moveForward(Events event);
+	/**
+	 * Keeps the robot waiting for a while
+	 * @param event : an event to wait to resume the behavior
+	 */
+	public void wait(Events event);
 
-    /**
-     * Moves the robot backward
-     * @param seconds : the number of seconds to move
-     */
-    public void moveBackward(int seconds);
-    
-    /**
-     * Moves the robot backward
-     * @param event : an event to wait to go on the next step
-     */
-    public void moveBackward(Events event);
- 
-    /**
-     * Turns the robot for 90 degrees to the right
-     */
-    public void turnRight();
+	/**
+	 * Moves the robot forward
+	 * @param seconds : the number of seconds to move
+	 */
+	public void moveForward(int seconds);
 
-    /**
-     * Turns the robot for 90 degrees to the left
-     */
-    public void turnLeft();
-    
-    /**
-     * Stops instantly the robot behavior
-     */
-    public void stop();
+	/**
+	 * Moves the robot forward
+	 * @param event : an event to wait to go on the next step
+	 */
+	public void moveForward(Events event);
+
+	/**
+	 * Moves the robot backward
+	 * @param seconds : the number of seconds to move
+	 */
+	public void moveBackward(int seconds);
+
+	/**
+	 * Moves the robot backward
+	 * @param event : an event to wait to go on the next step
+	 */
+	public void moveBackward(Events event);
+
+	/**
+	 * Turns the robot for 90 degrees to the right
+	 */
+	public void turnRight();
+
+	/**
+	 * Turns the robot for 90 degrees to the left
+	 */
+	public void turnLeft();
+
+	/**
+	 * Stops instantly the robot behavior
+	 */
+	public void stop();
 
 }
--- src/robdev/Events.java
+++ src/robdev/Events.java
@@ -1,12 +1,12 @@
 package robdev;
 
 /**
- * This enumeration represents all the events that the robot can detect.
+ * This enumeration represents all the events the robot can detect.
  */
 public enum Events {
 	
 	/**
-	 * A shock is detected, the robot met an obstacle, or somebody hits it!
+	 * A shock is detected, the robot has probably met an obstacle, or a foot kick ...
 	 */
 	SHOCK_DETECTED;
 
