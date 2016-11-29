0a565c2b308b4e8adc92f60ba3f807cb15fbf771
clementgeorge <clementyj.george@gmail.com>
2016-11-29 16:25:59 +0100

    new branch RobDevV3 containing a possible implementation for RobDev third version

--- src/robdev/Behavior.java
+++ src/robdev/Behavior.java
@@ -6,6 +6,8 @@ package robdev;
 public interface Behavior{
 
     /**
+     * This method is the starting point for your robot behavior. Use it like a 'main' 
+     * method. 
      * @param actions : a library containing all the actions the robot can perform
      */
     public void run(Actions actions);
