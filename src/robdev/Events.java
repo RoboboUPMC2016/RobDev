2ff0578d25cc59c07f830ddb2728ebdcf2bb84ec
clementgeorge <clementyj.george@gmail.com>
2016-12-03 18:59:07 +0100

    add when instruction

--- src/robdev/Events.java
+++ src/robdev/Events.java
@@ -3,11 +3,21 @@ package robdev;
 /**
  * This enumeration represents all the events the robot can detect.
  */
-public enum Events {
+public interface Events {
 	
 	/**
 	 * A shock is detected, the robot has probably met an obstacle, or a foot kick ...
 	 */
-	SHOCK_DETECTED;
+	public int SHOCK_DETECTED = 0;
+	
+	/**
+	 * Allows to define a specific behavior to execute if an event is detected
+	 * @param event: 
+	 * @param function:
+	 * @return an id allowing to cancel the listener
+	 */
+	public int when(int event, Runnable function);
+	
+	public void stop(int listener);
 
 }
